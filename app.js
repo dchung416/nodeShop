8380547905890fbf0df3e9451788c43c2a8ae485
dchung416 <dchung416@gmail.com>
2018-12-04 19:24:38 -0800

    completed up lecture 84

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/admin', adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+  // res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+  res.status(404).render('404', { docTitle: 'Page Not Found' })
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../helpers/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('shop.js', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop', path: '/' } );
 });
 
 module.exports = router;
