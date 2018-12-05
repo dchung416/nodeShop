adb6cd9cc7dbc6fbfeec79cc3861340e15635af6
dchung <dchung416@gmail.com>
2018-12-05 07:50:50 -0800

    completed up to lecture 90

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+//This is for the handlebars template engine
+// const expressHbs = require('express-handlebars'); 
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+//This command is for the pug template engine
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
+//The following commands are for the handlebars template engine
+// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
+
+//The following commands are for the ejs template engine
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
