2f3d4a5e7319e0db29cc1f5bff629adf05749987
dchung <dchung416@gmail.com>
2018-12-06 23:51:54 -0800

    completed up to lecture 163

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./helpers/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 //This is for the handlebars template engine
 // const expressHbs = require('express-handlebars'); 
@@ -47,10 +49,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User), { constraints: true, onDelete: 'CASCADE' };
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-// .sync({ force: true })
-.sync()
+.sync({ force: true })
+// .sync()
 .then(result => {
   // console.log(result)
   return User.findByPk(1)
--- controllers/admin.js
+++ controllers/admin.js
@@ -52,8 +52,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -111,7 +113,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
   .then(products => {
     res.render('admin/products', {
       prods: products,
